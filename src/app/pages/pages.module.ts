728d440203d548a5a5b4065f66b7dbd193ffad0c
Momfus <momfus@outlook.com>
2021-07-05 17:50:41 -0300

    clase 47 - uso de atributos personalizados

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { RouterModule } from '@angular/router'; // Solo necesitamos el router-oulet que proporciona este módulo (y no hace falta importar nuestro AppRoutingModule)
+import { FormsModule } from '@angular/forms';
 
 
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -9,7 +9,6 @@ import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
 import { SharedModule } from '../shared/shared.module';
-
 
 
 @NgModule({
@@ -27,6 +26,7 @@ import { SharedModule } from '../shared/shared.module';
   ],
   imports: [
     CommonModule,
+    FormsModule, // Para manejo de module (en este caso solo se usa en pages.module sino habría que generalizarlo en app.module o donde también se use)
     SharedModule,
     RouterModule
   ]
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
-  styleUrls: ['./progress.component.css']
+  styleUrls: ['./progress.component.css'],
 })
-export class ProgressComponent implements OnInit {
+export class ProgressComponent {
 
-  constructor() { }
+  progreso: number = 50;
 
-  ngOnInit(): void {
+  cambiarValor( valor: number): number {
+
+    if( this.progreso >= 100 && valor >= 0 ) {
+      return this.progreso = 100; // Mantiene el valor a 100 al superarlo y sale de la función
+    }
+
+    if( this.progreso <= 0 && valor < 0 ) {
+      return this.progreso = 0; // En caso de bajarse el valor a menos cero, se queda en cero, sino continua
+    }
+
+    return this.progreso = this.progreso + valor;
+
+  }
+
+  // Getters
+  get getPorcentaje() {
+    return `${this.progreso}%`
   }
 
 }
