e46740747a589fd314b26a39d128a6d666599ee7
Momfus <momfus@outlook.com>
2021-12-06 19:20:22 -0300

    Fin sección 8

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { ActivationEnd, Router } from '@angular/router';
+import { Component, OnDestroy } from '@angular/core';
+import { ActivatedRoute, ActivationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { filter, map } from 'rxjs/operators';
 
 @Component({
@@ -8,28 +9,34 @@ import { filter, map } from 'rxjs/operators';
   styles: [
   ]
 })
-export class BreadcrumbsComponent {
+export class BreadcrumbsComponent implements OnDestroy {
 
   public titulo!: string; // El símbolo "!" es para que no haga falta asignarle valor al momento de declararlo
+  public tituloSubs$!: Subscription;
 
-  constructor( private router: Router ) {
-    this.getArgumentosRuta();
+  constructor( private router: Router) {
+
+    this.tituloSubs$ = this.getArgumentosRuta()
+                        .subscribe( ({title}) => { // con ({title}) desesctructuramos el dato para tener el valor que queremos
+
+                          this.titulo = title;
+                          document.title = `AdminPro - ${title}`; // Así cambia el nombre de la pestaña del navegador
+
+                        });
+  }
+
+  ngOnDestroy(): void {
+    this.tituloSubs$.unsubscribe();
   }
 
   getArgumentosRuta() {
 
-    this.router.events
+    return this.router.events
     .pipe(
       filter<any>(event => event instanceof ActivationEnd),
       filter( (event2: ActivationEnd) => event2.snapshot.firstChild === null ), // Obtener el primero que es el padre con el título que requerimos de la data
       map( (event: ActivationEnd)  => event.snapshot.data)
     )
-    .subscribe( ({title}) => { // con ({title}) desesctructuramos el dato para tener el valor que queremos
-
-      this.titulo = title;
-      document.title = `AdminPro - ${title}`; // Así cambia el nombre de la pestaña del navegador
-
-    });
 
   }
 
