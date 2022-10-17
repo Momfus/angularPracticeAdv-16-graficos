047178700b8a8e79ae505d04edbd7f2ea5c18681
Momfus <momfus@outlook.com>
2022-10-17 16:22:05 -0300

    Validar las contraseñas iguales - Clase 165

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -13,10 +13,13 @@ export class RegisterComponent {
   public registerForm = this.fb.group({
     nombre: ['', Validators.required],
     email: ['', [Validators.required, Validators.email]],
-    password1: ['', Validators.required],
-    password2: ['', Validators.required],
+    password1: ['123456', Validators.required],
+    password2: ['123456', Validators.required],
     terminos: [ false, Validators.required]
 
+  },{
+    // Mis validadores personalizados
+    validators: this.passwordsIguales('password1', 'password2')
   });
 
   constructor( private fb: FormBuilder) { }
@@ -24,7 +27,7 @@ export class RegisterComponent {
   crearUsuario() {
 
     this.formSubmitted = true;
-    console.log( this.registerForm.value );
+    console.log( this.registerForm ); // Para errores debo fijarme en controls -> el campo que quiero ver -> errors
 
     if( this.registerForm.valid ) {
       console.log('posteando formulario');
@@ -44,8 +47,40 @@ export class RegisterComponent {
 
   }
 
+  contraseniasNoValidas() {
+
+    const pass1 = this.registerForm.get('password1')?.value;
+    const pass2 = this.registerForm.get('password2')?.value;
+
+    if( (pass1 !== pass2) && this.formSubmitted){
+      return true;
+    } else {
+      return false;
+    }
+
+  }
+
   aceptaTerminos() {
     return !this.registerForm.get('terminos')?.value && this.formSubmitted;
+  }
+
+  passwordsIguales(pass1Name: string, pass2Name: string) {
+
+    return( formGroup: FormGroup) => { // Obtengo lo nombres de los formularios y devuelve la condición como objeto
+
+      const pass1Control = formGroup.get(pass1Name);
+      const pass2Control = formGroup.get(pass2Name);
+
+      if( pass1Control?.value === pass2Control?.value) {
+
+        pass2Control?.setErrors(null)
+
+      } else {
+        pass2Control?.setErrors({ noEsIgual: true })
+      }
+
+    }
+
   }
 
 }
