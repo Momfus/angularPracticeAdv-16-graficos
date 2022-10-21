fb1f232dc215467ab79f9f5bbe7e7a0092e4b170
Momfus <momfus@outlook.com>
2022-10-21 15:28:48 -0300

    Guardar información del token en localStorage - Clase 170

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'; // Para manejos de pedidos http
-import { RegisterForm } from '../interfaces/register-form.interface';
 import { environment } from '../../environments/environment';
+import { tap } from 'rxjs/operators'; // tap es un operador de pipe para disparar un efecto secundario
+
+
+import { RegisterForm } from '../interfaces/register-form.interface';
 import { LoginForm } from '../interfaces/login-form.interface';
 
 const base_url = environment.base_url
@@ -15,14 +18,28 @@ export class UsuarioService {
 
   crearUsuario( formData: RegisterForm) {
 
-    return this.http.post(`${base_url}/usuarios`, formData); // Al ser un observable, la única forma que se dispare es que se debe subscribir donde se use (por eso el return)
+    return this.http.post(`${base_url}/usuarios`, formData) // Al ser un observable, la única forma que se dispare es que se debe subscribir donde se use (por eso el return)
+                .pipe(
+                  tap( (res: any) => {
+                    // Efecto secundario al suscribirse
+                    // Guardar el token en el localstorage
+                    localStorage.setItem('token', res.token)
 
+                  })
+                );
   }
 
   loginUsuario( formData: LoginForm) {
 
-    return this.http.post(`${base_url}/login`, formData); // Al ser un observable, la única forma que se dispare es que se debe subscribir donde se use (por eso el return)
+    return this.http.post(`${base_url}/login`, formData) // Al ser un observable, la única forma que se dispare es que se debe subscribir donde se use (por eso el return)
+                .pipe(
+                  tap( (res: any) => {
+                    // Efecto secundario al suscribirse
+                    // Guardar el token en el localstorage
+                    localStorage.setItem('token', res.token)
 
+                  })
+                );
   }
 
 }
