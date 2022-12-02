79b586a59244921ad49cfbdde180945328c10c8a
Momfus <momfus@outlook.com>
2022-12-01 21:46:55 -0300

    modal para la carga deimagenes base - clase 208

---
+++ src/app/components/modal-imagen/modal-imagen.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-imagen',
+  templateUrl: './modal-imagen.component.html',
+  styles: [
+  ]
+})
+export class ModalImagenComponent {
+
+  public ocultarModal: boolean = false;
+
+
+  cerrarModal() {
+    this.ocultarModal = true;
+  }
+
+}
