25fa272128ae437349b48eda388da57536088060
Momfus <momfus@outlook.com>
2022-12-03 16:21:41 -0300

    pipes para mostrar las imagenes - clase 219

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -6,6 +6,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 // Módulos
 import { SharedModule } from '../shared/shared.module';
 import { ComponentsModule } from '../components/components.module';
+import { PipesModule } from '../pipes/pipes.module';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
@@ -47,7 +48,8 @@ import { MedicosComponent } from './mantenimientos/medicos/medicos.component';
     SharedModule,
     RouterModule,
     ComponentsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    PipesModule
   ]
 })
 export class PagesModule { }
---
+++ src/app/pipes/imagen.pipe.ts
@@ -0,0 +1,34 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { environment } from '../../environments/environment.prod';
+
+const base_url = environment.base_url;
+
+@Pipe({
+  name: 'imagen'
+})
+export class ImagenPipe implements PipeTransform {
+
+  transform(img: string | undefined, tipo: 'usuariois'|'medicos'|'hospitales'): string {
+
+    if( !img){ // en caso que no exista el campo por alguna razón
+
+      return `${base_url}/upload/usuarios/no-image`;
+
+    } else if( img.includes('https') ) { // Para imagen de login de google
+
+      return img;
+
+    } else if( img ) { // Resto de los casos
+
+      return `${base_url}/upload/${tipo}/${img}`;
+
+    } else {
+
+      return `${base_url}/upload/usuarios/no-image`;
+
+    }
+
+
+  }
+
+}
